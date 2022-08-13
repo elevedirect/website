9afc0f28f8870aff05ac85114d1e724fa99e64b0
CAMARM_Flipz <armand.camponovo@icloud.com>
2022-08-13 20:43:12 +0200

    startiong website

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+from flask import Flask, render_template
+
+app = Flask('EleveDirect Website')
+
+
+@app.route('/')
+def index():
+    news_title, news_content = requests.get('https://raw.githubusercontent.com/elevedirect/app/master/CHANGELOG.md').content.decode().split('---')
+    return render_template('index.html', title=news_title, content=news_content)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9090, host='0.0.0.0')
